4bae59753ee1c1db3b050ae9daef3bb67485ad6f
Harold-Chan <cch.harold@gmail.com>
2024-02-07 00:26:17 -0500

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def static_view(request):
+    return render(request, 'static.html')
 
 # Create an `about` view to render a static about page
 # def about(request):
